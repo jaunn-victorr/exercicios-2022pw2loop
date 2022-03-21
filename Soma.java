76713fd36c2efedd1428d9b7423fbcc806d211d9
jaunn-victorr <joao.vitor.ptal@gmail.com>
2022-03-21 17:55:46 +0000

    Exercício01

---
+++ Soma.java
@@ -0,0 +1,10 @@
+public class Soma{
+    public static void main (String[] args) {
+        // percorre os números de 1 à 500
+        for(int num = 1;num <= 500;num++) {
+            //verificar se o número é ímpar
+            if ((num % 2) !=0)
+            System.out.println(num);
+        }
+    }
+}
